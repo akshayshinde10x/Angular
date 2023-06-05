2d5f39bbf03619cfc14c6f608fcdcc233a758594
Akshay Shinde <akshayshinde281195@gmail.com>
2023-06-05 09:10:23 +0530

    Update app.module.ts

--- CRM/src/app/app.module.ts
+++ CRM/src/app/app.module.ts
@@ -6,19 +6,25 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './home/header/header.component';
 import { ReportComponent } from './home/report/report.component';
+import { FormsModule } from '@angular/forms';
+import { StudentComponent } from './home/student/student.component';
+import { AuthGuard } from './AuthGuard/auth-guard.service';
+import { AuthService } from './AuthGuard/auth.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    ReportComponent
+    ReportComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [AuthGuard, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
