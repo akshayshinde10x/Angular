627bf7d92dc652f4568337ef2ec5a8a55e130968
Akshay Shinde <akshayshinde281195@gmail.com>
2023-06-05 09:09:53 +0530

    Update app-routing.module.ts

--- CRM/src/app/app-routing.module.ts
+++ CRM/src/app/app-routing.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ReportComponent } from './home/report/report.component';
+import { StudentComponent } from './home/student/student.component';
+import { AuthGuard } from './AuthGuard/auth-guard.service';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'report', component: ReportComponent},
+  {path: 'student',  canActivateChild: [AuthGuard], component:StudentComponent,
+    children:[
+      {path: ':id/:name', component: StudentComponent},
+      {path: 'edit', component: StudentComponent},
+    ]  
+  }
 ];
 
 @NgModule({
